ec3b5f7231ee2bf55a2ad77062e865f61295a02f
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-02 10:43:10 +0530

    Update RemoveFavourites.jsx

--- src/components/RemoveFavourites.jsx
+++ src/components/RemoveFavourites.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const RemoveFavourites = () => {
   return (
-    <span className="text-white text-xs sm:text-s text-center curser-pointer ease-in-out duration-300">
-      Remove from favourits🗑️
+    <span className="text-white text-xs sm:text-sm text-center cursor-pointer ease-in-out duration-300 hover:text-red-500">
+      Remove from favorites 🗑️
     </span>
   );
 };

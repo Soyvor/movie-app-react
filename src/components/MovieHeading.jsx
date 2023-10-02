29c0ce79e6837401516b970f6f043d791e0d860b
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-02 10:42:47 +0530

    Update MovieHeading.jsx

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const MovieHeading = (props) => {
   return (
     <div>
-      <h1 className="text-2xl text-white py-4 px-3 font-bold">{props.heading}</h1>
+      <h1 className="text-2xl text-white py-4 px-3 font-bold">
+        {props.heading}
+      </h1>
     </div>
   );
 };

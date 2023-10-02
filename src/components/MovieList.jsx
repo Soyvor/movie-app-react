15e5514d896b3d689b2a5d9b553863a3d27b639c
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-02 10:42:59 +0530

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,22 @@ import React from "react";
 
 const MovieList = (props) => {
   const FavouriteComponent = props.favouriteComponent;
+
   return (
     <div>
-      <div className="flex items-center">
+      <div className="flex flex-wrap justify-center -mx-2">
         {props.movies.map((movie) => (
-          <div className="w-[200px]   inline-block cursor-pointer hover:scale-105 ease-in-out duration-300 mx-1">
-            <img className="object-cover" src={movie.Poster} alt="poster-img" />
+          <div
+            key={movie.imdbID}
+            className="w-[200px] sm:w-[250px] p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
+          >
+            <img
+              className="object-cover rounded-lg"
+              src={movie.Poster}
+              alt="poster-img"
+            />
             <div
-              className="w-full h-full flex  justify-center items-center backdrop-brightness-50 p-3"
+              className="w-full h-12 bg-opacity-50 backdrop-blur backdrop-brightness-50 absolute bottom-0 left-0 flex justify-center items-center transition-opacity duration-300 opacity-0 hover:opacity-100"
               onClick={() => props.handleFavouriteClick(movie)}
             >
               <FavouriteComponent />

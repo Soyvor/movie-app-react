5599b3060727c13a3cf64207b73b153cce8653e8
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-02 10:43:26 +0530

    Update SearchBox.jsx

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const SearchBox = (props) => {
   return (
     <div className="p-4">
-      <input className="py-2" type="text" value={props.value} onChange={(event) => props.setSearchValue(event.target.value)} placeholder="Serach a movie" />
+      <input
+        className="py-2 px-4 rounded-full focus:outline-none focus:ring focus:border-blue-300 transition duration-300 w-full sm:w-64"
+        type="text"
+        value={props.searchValue}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Search a movie"
+      />
     </div>
   );
 };
